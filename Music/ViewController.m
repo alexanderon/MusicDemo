cbe9b175e19c45575b49503d02f0582bb7254368
RAHUL JOSHI <rahul1994joshi@gmail.com>
2015-09-22 14:32:33 +0530

    Initial Commit

---
+++ Music/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Music
+//
+//  Created by RAHUL on 9/22/15.
+//  Copyright (c) 2015 RAHUL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
